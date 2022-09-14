c9496c774567dc619094817a92ef414ba4167a91
the-zmaze <thezmaze@protonmail.com>
2022-09-14 22:06:10 +0200

    Implement actions requiring a vote (parliament NYI)

--- political_idea_1/core.py
+++ political_idea_1/core.py
@@ -15,6 +15,15 @@ class Action:
             except AttributeError:
                 raise UndefinedAction(f"{cls.code} "
                                       + "has no execution process defined")
+        elif cls.code in (
+                country.constitution.authorities[authority]["can_propose"]):
+            try:
+                # TODO replace playmobils with a fully functionnal parliament
+                print("The playmobils have voted, the reform was approved.")
+                cls.execute(authority, country, **kwargs)
+            except AttributeError:
+                raise UndefinedAction(f"{cls.code} "
+                                      + "has no execution process defined")
         else:
             raise UnauthorizedAction(f"{authority} isn't qualified for "
                                      + f"{cls.code} according to "
--- political_idea_1/political.py
+++ political_idea_1/political.py
@@ -19,6 +19,15 @@ class GrossMinimumWage(Action):
         country.gross_minimum_wage = kwargs["value"]
 
 
+class InheritanceTax(Action):
+    code = "inheritance_tax"
+    desc = "Update the Inheritance Tax"
+
+    @classmethod
+    def execute(cls, authority, country, **kwargs):
+        country.Taxes.inheritance = kwargs["value"]
+
+
 class DevConstitution(Constitution):
     code = "dev_constitution"
     authorities = {
@@ -28,7 +37,17 @@ class DevConstitution(Constitution):
         },
         "prime_minister": {
             "name": "Premier.ère Ministre",
-            "qualif": ["hello_world", "gmw"]
+            "qualif": ["hello_world", "gmw"],
+            "can_propose": ["inheritance_tax"]
+        },
+        "government": {
+            "name": "Gouvernement Dev I",
+            "ministries": {
+                "ecology": {
+                    "name": "Ministère de la Transition Écologique",
+                    "minister_name": "Ministre de la Transition Écologique"
+                }
+            }
         }
     }
 
@@ -45,7 +64,30 @@ class DevCountry(Country):
 
     gross_minimum_wage = 1500
 
+    class Taxes:
+        inheritance = {
+            150000: 10  # The first layer start at 150000 and is a tax of 10%
+        }
+
 
 HelloWorld.order("president", DevCountry)
+
 GrossMinimumWage.order("prime_minister", DevCountry, value=1700)
 print(DevCountry.gross_minimum_wage)
+
+InheritanceTax.order("prime_minister",
+                     DevCountry,
+                     value={
+                         500000: 5,
+                         1000000: 10,
+                         2000000: 20,
+                         3000000: 30,
+                         4000000: 40,
+                         5000000: 50,
+                         6000000: 60,
+                         7000000: 70,
+                         8000000: 80,
+                         9000000: 90,
+                         10000000: 100
+                     })
+print(DevCountry.Taxes.inheritance)
